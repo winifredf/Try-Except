610664b316f58a749941c3543216a7fe8066333b
Winifredf <Winifred.Dykes@hotmail.com>
2023-04-16 12:48:43 -0400

    updated err to print specific error lines 5-6

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ value = 10/0
 try:
     number = int(input("Enter a number: "))
     print(number)
-except ZeroDivisionError:
-    print("Divided by zero")
+except ZeroDivisionError as err:
+    print("err")
 except ValueError:
     print("Invalid Input")
